662f1957272134a84ec94c7eef558a23a6a03fa6
Anton Berezin <gurunars@gmail.com>
2015-02-10 11:55:00 +0200

    3: do not let start/stop if it is already started/stopped

--- state_machine_crawler/webview.py
+++ state_machine_crawler/webview.py
@@ -111,10 +111,14 @@ class WebView(object):
             httpd.handle_request()
 
     def start(self):
+        if self._alive:
+            return
         self._alive = True
         self._viewer_thread.start()
 
     def stop(self):
+        if not self._alive:
+            return
         self._alive = False
         urllib2.urlopen("http://%s:%d/kill" % (self.HOST, self._server.server_port))
         self._viewer_thread.join()
